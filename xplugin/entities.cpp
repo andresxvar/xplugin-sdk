3fb5ef7ff838abab393d3bca917a8eea1ed13a93
weaponx <avaero6@gmail.com>
2015-09-21 15:15:12 -0700

    clean up and commenting

--- xplugin/entities.cpp
+++ xplugin/entities.cpp
@@ -11,20 +11,36 @@ target:			target fired for all clients
 */
 void Lmdp_Trigger_Use(gentity_t *self, gentity_t *other, gentity_t *activator)
 {
-	if (self->target && self->target[0])
-	{
-		gentity_t *player = NULL;
-		for (int i = 0; i < g_maxclients; ++i) {
-			player = ENT_FROM_NUM(i);
-			if (player->client->pers.connected == CON_CONNECTED
-				&& PlayerUseableCheck(self, player))
-				LmdApi.entities->useTargets(self, player, self->target);
-		}
+	gentity_t *player = NULL;
+	
+	// it is not necessary to check for a valid target, since
+	// this entity can only exist with a target and targetname
+
+	for (int i = 0; i < g_maxclients; ++i) {
+		player = ENT_FROM_NUM(i);
+		if (player->client->pers.connected == CON_CONNECTED
+			&& PlayerUseableCheck(self, player))
+			LmdApi.entities->useTargets(self, player, self->target);
 	}
 }
 void Lmdp_Trigger_Spawn(gentity_t *self)
 {
-	self->use = Lmdp_Trigger_Use;
+	if (self->target && self->targetname) {
+
+		// set use function
+		self->use = Lmdp_Trigger_Use;
+	}
+	else
+	{
+		// report failure to spawn
+		LmdApi.entities->reportSpawnFailure(
+			"Lmdp_Trigger_Spawn: entitiy must have target and targetname");
+		
+		// free the entity
+		LmdApi.entities->freeEntity(self);		
+	}
+
+	// clear spawn keys
 	LmdApi.entities->clearSpawnKeys();
 }
 
--- xplugin/hook.cpp
+++ xplugin/hook.cpp
@@ -6,16 +6,18 @@
 
 Hook::Hook(unsigned int oldPtr, unsigned int newPtr)
 {
-	mOldPtr = oldPtr;
-	mNewPtr = newPtr;
+	mOldPtr = oldPtr; // old function address
+	mNewPtr = newPtr; // new function address
 
-	memcpy( mOriginalBytes, (void *)oldPtr, 6 );
+	memcpy( mOriginalBytes, (void *)oldPtr, 6 ); // store original bytes
 }
 
+// Patch
+// write new bytes to add hook
 qboolean Hook::Patch()
 {
-	unsigned int calcAdr;
-	unsigned char newBytes[6];
+	unsigned int calcAdr;	// function body address
+	unsigned char newBytes[6]; // bytes to write
 	unsigned long dwOldProtect = 0;
 
 	if (VirtualProtect((void*)mOldPtr, 6,
@@ -25,14 +27,16 @@ qboolean Hook::Patch()
 		calcAdr = mNewPtr - (mOldPtr + 5);
 		memcpy(&newBytes[1], &calcAdr, 4);
 		newBytes[5] = 0xC3; // retn
-		memcpy((void*)mOldPtr, &newBytes, 6);
+		memcpy((void*)mOldPtr, &newBytes, 6); // write new bytes
 		VirtualProtect((void*)mOldPtr, 6, dwOldProtect, &dwOldProtect);
 		return qtrue;
 	}
 	else
-		return qfalse;
+		return qfalse; // failed
 }
 
+// RemovePatch
+// write original bytes to remove hook
 qboolean Hook::RemovePatch()
 {
 	unsigned long dwOldProtect = NULL;
@@ -40,10 +44,10 @@ qboolean Hook::RemovePatch()
 	if (VirtualProtect((void*)mOldPtr, 6,
 		PAGE_EXECUTE_READWRITE, &dwOldProtect))
 	{
-		memcpy((void *)mOldPtr, mOriginalBytes, 6 );
+		memcpy((void *)mOldPtr, mOriginalBytes, 6 ); // write original bytes
 		VirtualProtect((void*)mOldPtr, 6, dwOldProtect, &dwOldProtect);
 		return qtrue;
 	}
 	else
-		return qfalse;
+		return qfalse; // failed
 }
